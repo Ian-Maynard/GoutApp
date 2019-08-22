fb2f55802753e727f2e76c5aa9196d11ce72c7b6
Ian-Maynard <maynard.ian@gmail.com>
2019-08-22 18:39:50 -0400

    Adapting routes from exercises

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* jshint esversion: 6 */ 
+/* jshint esversion: 8 */ 
 
 const indexDebugger = require('debug')('app:index');
 const dbDebugger = require('debug')('app:db');
@@ -12,7 +13,11 @@ const helmet = require ("helmet");
 const config = require ("config");
 const ingredients = require("./routes/ingredients.js");
 const main = require("./routes/main.js");
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/goutDB')
+ .then(() => console.log('Connected  to Mongodb...'))
+ .catch(err => console.error('Could not connect to MongoDB...',err));
 
 app.use(express.json()); // Parses JSON objects. Not a default function
 app.use(helmet()); // Helmet secures HTTP request
@@ -31,6 +36,4 @@ app.set('views', './views/layouts');
 //     startupDebugger("Environment is set to Development, therefore Morgan logging is enabled");
 // } // Morgan Logs requests and should only be used in Dev environments or for troubleshooting.
 
-
 app.listen(port, () => console.log(`Listening on port ${port}`)); // Event handler 
-
